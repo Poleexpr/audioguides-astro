8c518d0524abdfd2bdbf4a4af260b981803b51ae
Anya Cherkasova <a.c4erkasova@gmail.com>
2024-08-12 20:38:12 +0300

    feat: add the footer

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,7 @@
 export { default as IconArrowDown } from "./IconArrowDown.astro";
+export { default as IconArrowUp } from "./IconArrowUp.astro";
+export { default as IconLogoVk } from "./IconLogoVk.astro";
+export { default as IconLogoTg } from "./IconLogoTg.astro";
+export { default as IconLogoRd } from "./IconLogoRd.astro";
+export { default as IconLogoFb } from "./IconLogoFb.astro";
+export { default as IconLogoAudioguides } from "./IconLogoAudioguides.astro";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./welcome";
 export * from "./icons";
+export * from "./footer";
